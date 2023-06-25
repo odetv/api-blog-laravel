f611720a839dbbc814471787351f8365040c504c
odetv <abdiutama02@gmail.com>
2023-06-25 12:49:34 +0800

    updated routes use group middleware

--- routes/api.php
+++ routes/api.php
@@ -5,9 +5,12 @@ use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/posts', [PostController::class, 'index'])->middleware(['auth:sanctum']);
-Route::get('/posts/{id}', [PostController::class, 'showDetails'])->middleware(['auth:sanctum']);
-
 Route::post('/login', [AuthenticationController::class, 'login']);
-Route::get('/logout', [AuthenticationController::class, 'logout'])->middleware(['auth:sanctum']);
-Route::get('/me', [AuthenticationController::class, 'me'])->middleware(['auth:sanctum']);
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/logout', [AuthenticationController::class, 'logout']);
+    Route::get('/me', [AuthenticationController::class, 'me']);
+});
+
+Route::get('/posts', [PostController::class, 'index']);
+Route::get('/posts/{id}', [PostController::class, 'showDetails']);
