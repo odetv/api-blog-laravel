9f2a31cfb6a8aa61f6d2952d3b2c449227208250
odetv <abdiutama02@gmail.com>
2023-07-04 23:09:05 +0800

    updated

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -7,9 +7,34 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpFoundation\Response;
 
 class AuthenticationController extends Controller
 {
+    // public function login(Request $request)
+    // {
+    //     $request->validate([
+    //         'email' => 'required|email',
+    //         'password' => 'required'
+    //     ]);
+
+    //     $user = User::where('email', $request->email)->first();
+
+    //     if (!$user || !Hash::check($request->password, $user->password)) {
+    //         throw ValidationException::withMessages([
+    //             'email' => ['The provided credentials are incorrect.'],
+    //         ]);
+    //     }
+
+    //     return $user->createToken('user-login')->plainTextToken;
+    // }
+
+    // public function logout(Request $request)
+    // {
+    //     $request->user()->currentAccessToken()->delete();
+    //     return "Berhasil logout!";
+    // }
+
     public function login(Request $request)
     {
         $request->validate([
@@ -17,25 +42,36 @@ class AuthenticationController extends Controller
             'password' => 'required'
         ]);
 
-        $user = User::where('email', $request->email)->first();
+        $credentials = $request->only('email', 'password');
 
-        if (!$user || !Hash::check($request->password, $user->password)) {
-            throw ValidationException::withMessages([
-                'email' => ['The provided credentials are incorrect.'],
-            ]);
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            $token = $user->createToken('api-token')->plainTextToken;
+
+            return response()->json(['token' => $token]);
         }
 
-        return $user->createToken('user-login')->plainTextToken;
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
     }
 
     public function logout(Request $request)
     {
         $request->user()->currentAccessToken()->delete();
-        return "Berhasil logout!";
+
+        return response()->json(['message' => 'Logged out']);
     }
 
     public function me(Request $request)
     {
         return response()->json(Auth::user());
+    }
+
+    public function getUser()
+    {
+        return response()->json([
+            "user" => auth()->user()
+        ], Response::HTTP_OK);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -6,9 +6,16 @@ use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::get("/storage-link", function () {
+    $targetFolder = storage_path("app/public");
+    $linkFolder = $_SERVER['DOCUMENT_ROOT'] . '/storage';
+    symlink($targetFolder, $linkFolder);
+});
+
 Route::post('/login', [AuthenticationController::class, 'login']); //Login
 Route::middleware(['auth:sanctum'])->group(function () {
-    Route::get('/logout', [AuthenticationController::class, 'logout']); //Logout
+    Route::get('/user', [AuthenticationController::class, 'getUser']); //Get User
+    Route::post('/logout', [AuthenticationController::class, 'logout']); //Logout
     Route::get('/me', [AuthenticationController::class, 'me']); //Check Token API User
     Route::post('/posts', [PostController::class, 'store']); //Create Posts
     Route::post('/posts/{id}', [PostController::class, 'update'])->middleware('post-owner'); //Update Posts
